b4d32ba1fa59ba48f7c1c31d3b5155621810ec24
Sid <bhimanisiddharth5@gmail.com>
2026-04-17 08:37:39 +0100

    Add base function approximation class

--- agents/baseQAgent.py
+++ agents/baseQAgent.py
@@ -7,20 +7,22 @@ from .agent import Agent, assert_agent_set_up
 class BaseQValAgent(Agent):
     def __init__(
         self,
-        learning_rate: float = 0.2,
-        epsilon: float = 0.2,
-        learning_decay: float = 0.0,
-        epsilon_decay: float = 0.000002,
-        final_learning_rate: float = 0.2,
-        final_epsilon: float = 0.05,
-        discount_factor: float = 0.95
+        learning_rate: float,
+        epsilon: float,
+        learning_decay: float,
+        epsilon_decay: float,
+        final_learning_rate: float,
+        final_epsilon: float,
+        discount_factor: float
     ):
-        """Initialize a Q-Learning agent.
+        """Initialize a Q-Value based agent.
 
         Args:
             learning_rate: How quickly to update Q-values
-            initial_epsilon: Starting exploration rate
+            epsilon: Starting exploration rate
+            learning_decay: How much to decay the learning rate
             epsilon_decay: How much to reduce epsilon each episode
+            final_learning_rate: Minimum learning rate
             final_epsilon: Minimum exploration rate
             discount_factor: How much to value future rewards
         """
@@ -43,12 +45,12 @@ class BaseQValAgent(Agent):
     def set_up(self,
                action_space: Space,
                observation_space: Space | None = None,
-               seed=None):
+               seed=None) -> None:
         super().set_up(action_space, seed=seed)
         self.numActions = action_space.n
 
     @assert_agent_set_up
-    def get_action(self, obs: tuple, mask) -> int:
+    def get_action(self, obs: np.ndarray, mask: np.ndarray) -> int:
         if (self.learning) and (np.random.random() < self.epsilon):
             return self.action_space.sample(mask)
         else:
@@ -67,7 +69,7 @@ class BaseQValAgent(Agent):
             return random.choice(maxActions)
 
     @assert_agent_set_up
-    def update(self, reward: float, obs: tuple, action: int):
+    def update(self, reward: float, obs: np.ndarray, action: int) -> None:
         if self.learning and (self.prevObs is not None):
             next_q = self.get_next_q(obs, action)
             curr_q = self.get_q_value(self.prevObs, self.prevAction)
@@ -83,64 +85,137 @@ class BaseQValAgent(Agent):
         self.prevAction = action
 
     @assert_agent_set_up
-    def final(self, reward):
+    def final(self, reward: float) -> None:
         super().final(reward)
         self.decay()
 
     @assert_agent_set_up
-    def get_q_value(self, obs, action) -> float:
+    def get_q_value(self, obs: np.ndarray, action: int) -> float:
         pass
 
     @assert_agent_set_up
-    def get_max_q_value(self, obs) -> float:
+    def get_max_q_value(self, obs: np.ndarray) -> float:
         pass
 
     @assert_agent_set_up
-    def update_q_value(self, obs, action, new_q: float):
+    def update_q_value(self, obs: np.ndarray, action: int, new_q: float) -> None:
         self.num_updates += 1
 
     @assert_agent_set_up
-    def get_next_q(self, obs, action):
+    def get_next_q(self, obs: np.ndarray, action: int) -> float:
         pass
 
-    def decay(self):
+    def decay(self) -> None:
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
-        self.lr = max(self.final_lr, self.lr - self.lr_decay)
+        self.lr = max(self.final_lr, self.lr - self.lr_decay)      
 
-    @assert_agent_set_up
-    def get_next_q(self, obs, action):
-        pass
+class FuncApprox(BaseQValAgent):
+
+    def __init__(
+        self,
+        learning_rate: float = 1e-3,
+        epsilon: float = 1e-1,
+        learning_decay: float = 1e-8,
+        epsilon_decay: float = 1e-5,
+        final_learning_rate: float = 1e-4,
+        final_epsilon: float = 1e-2,
+        discount_factor: float = 0.95
+    ):
+
+        super().__init__(
+            learning_rate, epsilon, learning_decay, epsilon_decay,
+            final_learning_rate, final_epsilon, discount_factor
+        )
         
 
-class FuncApprox:
-    pass
+    def set_up(self, 
+               action_space: Space,
+               observation_space: Space,
+               seed=None) -> None:
+        super().set_up(action_space, seed=seed)
+        self.observation_space = observation_space
+        self.numFeatures = np.prod(observation_space.shape)
+        self.q_function = self.getDefaultFunc()
+
+    @assert_agent_set_up
+    def update(self, reward: float, obs: np.ndarray, action: int) -> None:
+        if self.learning and (self.prevObs is not None):
+            next_q = self.get_next_q(obs, action)
+            curr_q = self.get_q_value(self.prevObs, self.prevAction)
+            
+            target = reward + (self.discount_factor * next_q)
+            temporal_difference = target - curr_q
+            
+            obs_vector = self.obs_to_feature_vector(self.prevObs, self.prevAction)
+            self.q_function += (obs_vector * (self.lr * temporal_difference))
+            
+            self.training_error.append(temporal_difference)
+        
+        self.prevObs = obs
+        self.prevAction = action
+
+    @assert_agent_set_up
+    def get_q_value(self, obs: np.ndarray, action: int) -> float:
+        vector = self.obs_to_feature_vector(obs, action)
+        return vector @ self.q_function
+
+    @assert_agent_set_up
+    def get_max_q_value(self, obs: np.ndarray) -> float:
+        return max(self.get_q_value(obs, action) for action in range(self.numActions))
+
+    def obs_to_feature_vector(self, obs: np.ndarray, action: int) -> np.ndarray:
+        obs_vector = np.ravel(obs)
+        vector = np.zeros((self.q_function.size))
+        vector[action * self.numFeatures:(action + 1) * self.numFeatures] += obs_vector
+        vector[-1] += 1.0
+        return vector
+
+    def getDefaultFunc(self) -> np.ndarray:
+        size = self.numActions * self.numFeatures
+        return np.zeros(size + 1)
 
 class Tabular(BaseQValAgent):
     """
     Tabular q-value based agent
     Converts observations to bytes and adds to a dictionary with action utilities
     """
+    
+    def __init__(
+        self,
+        learning_rate: float = 2e-1,
+        epsilon: float = 2e-1,
+        learning_decay: float = 2e-6,
+        epsilon_decay: float = 2e-6,
+        final_learning_rate: float = 2e-2,
+        final_epsilon: float = 2e-2,
+        discount_factor: float = 0.95
+    ):
 
+        super().__init__(
+            learning_rate, epsilon, learning_decay, epsilon_decay,
+            final_learning_rate, final_epsilon, discount_factor
+        )
+    
     def set_up(self,
                action_space: Space,
                observation_space: Space | None = None,
-               seed=None):
+               seed=None) -> None:
         super().set_up(action_space, seed=seed)
         self.q_values = defaultdict(self.getDefaultVals)
 
     @assert_agent_set_up
-    def get_q_value(self, obs, action) -> float:
+    def get_q_value(self, obs: np.ndarray, action: int) -> float:
         return self.q_values[obs.tobytes()][action]
 
     @assert_agent_set_up
-    def get_max_q_value(self, obs) -> float:
+    def get_max_q_value(self, obs: np.ndarray) -> float:
         return np.max(self.q_values[obs.tobytes()])
     
     @assert_agent_set_up
-    def update_q_value(self, obs, action, new_q: float):
+    def update_q_value(self, obs: np.ndarray, action: int, new_q: float):
         super().update_q_value(obs, action, new_q)
         self.q_values[obs.tobytes()][action]= new_q
 
     @assert_agent_set_up
-    def getDefaultVals(self):
+    def getDefaultVals(self) -> np.ndarray:
         return np.array(list(0.0 for _ in range(self.numActions)), dtype=np.float16)
