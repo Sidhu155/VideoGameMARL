8457eb21db53bec73a3ad8279979d9cef35338e1
Sid <bhimanisiddharth5@gmail.com>
2026-04-22 13:30:47 +0100

    Add basics of evaluator file

--- evaluator.py
+++ evaluator.py
@@ -1,5 +1,10 @@
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
+from argparse import ArgumentParser
+from file import loadFromFile
+from agents.agent import Agent
+from environments.environment import Environment
 
 class Evaluator:
     def __init__(self):
@@ -14,3 +19,34 @@ class Evaluator:
     
     def show(self):
         plt.show()
+
+    def plotAgents(self, agents: list[Agent]):
+        pass
+
+    def plotEnvironments(self, environments: list[Environment]):
+        pass
+
+
+def main(args: list[str] | None = None):
+    parser = ArgumentParser()
+    parser.add_argument("type", choices=["environment", "agent"])
+    parser.add_argument("objects", nargs='+', type=str)
+    args = parser.parse_args(args)
+    eval = Evaluator()
+
+    if args.type == "environment":
+        for env in args.objects:
+            assert type(env) == Environment
+        eval.plotEnvironments(args.objects)
+    else:
+        for agent in args.objects:
+            assert type(env) == Agent
+        eval.plotAgents(args.objects)
+
+    eval.show()
+        
+
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
