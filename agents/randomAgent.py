c8e834db63c4f7110dd2556c4d804e409b9246b8
Sid <bhimanisiddharth5@gmail.com>
2026-04-24 09:15:38 +0100

    Add timing for get_action in random agent

--- agents/randomAgent.py
+++ agents/randomAgent.py
@@ -1,8 +1,9 @@
 from .agent import Agent
-from utils import assert_agent_set_up
+from utils import assert_agent_set_up, time_func
 
 class RandomAgent(Agent):
 
+    @time_func("get_action")
     @assert_agent_set_up
     def get_action(self, obs, mask) -> int:
         return self.action_space.sample(mask)
